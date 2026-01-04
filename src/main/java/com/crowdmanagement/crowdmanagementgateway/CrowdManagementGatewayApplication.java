2aa0fb441161c5eee3397d418129ad7a574e3502
AzamatAbraev <azamatabraev07@gmail.com>
2026-01-04 21:37:19 +0500

    Gateway initialized and testing for basic usage

---
+++ src/main/java/com/crowdmanagement/crowdmanagementgateway/CrowdManagementGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.crowdmanagement.crowdmanagementgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdManagementGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdManagementGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crowdmanagement/crowdmanagementgateway/CrowdManagementGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdmanagement.crowdmanagementgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdManagementGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
