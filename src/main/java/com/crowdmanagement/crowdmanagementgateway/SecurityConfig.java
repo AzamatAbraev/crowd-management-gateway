3de1a454eb9af0714627a27a3d54b7924601ca0c
AzamatAbraev <azamatabraev07@gmail.com>
2026-01-05 21:09:16 +0500

    logout functionality added: basic version

--- src/main/java/com/crowdmanagement/crowdmanagementgateway/SecurityConfig.java
+++ src/main/java/com/crowdmanagement/crowdmanagementgateway/SecurityConfig.java
@@ -4,20 +4,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
+    private final ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfig(ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-                .authorizeExchange(exchange -> exchange
-                        .anyExchange().authenticated()
-                )
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/demo/**").authenticated()
+                        .pathMatchers("/analytics/**").authenticated()
+                        .anyExchange().permitAll()
+                )
                 .oauth2Login(Customizer.withDefaults())
-                .oauth2Client(Customizer.withDefaults());
+                .oauth2Client(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                );
+
 
         return http.build();
+    }
+
+    @Bean
+    public ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedServerLogoutSuccessHandler handler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(
+                        clientRegistrationRepository
+                );
+
+        handler.setPostLogoutRedirectUri("{baseUrl}/");
+
+        return handler;
     }
 }
