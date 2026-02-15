02037d19ed3f3fc4f6963d89a86bc47f6d810e72
AzamatAbraev <azamatabraev07@gmail.com>
2026-02-15 17:21:42 +0500

    established communication with react spa

--- src/main/java/com/crowdmanagement/crowdmanagementgateway/SecurityConfig.java
+++ src/main/java/com/crowdmanagement/crowdmanagementgateway/SecurityConfig.java
@@ -2,12 +2,22 @@ package com.crowdmanagement.crowdmanagementgateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -21,14 +31,20 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/demo/**").authenticated()
-                        .pathMatchers("/analytics/**").authenticated()
-                        .anyExchange().permitAll()
+                        .pathMatchers("/login/**", "/oauth2/**").permitAll()
+                        .pathMatchers("/ws/**").permitAll()
+                        .pathMatchers("/resources/**").authenticated()
+                        .anyExchange().authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(oauth2 -> oauth2
+                        .authenticationSuccessHandler(
+                                new RedirectServerAuthenticationSuccessHandler("http://localhost:5173/home")
+                        ))
                 .oauth2Client(Customizer.withDefaults())
                 .logout(logout -> logout
+                        .requiresLogout(ServerWebExchangeMatchers.pathMatchers(HttpMethod.GET, "/logout"))
                         .logoutSuccessHandler(oidcLogoutSuccessHandler())
                 );
 
@@ -43,8 +59,21 @@ public class SecurityConfig {
                         clientRegistrationRepository
                 );
 
-        handler.setPostLogoutRedirectUri("{baseUrl}/");
+        handler.setPostLogoutRedirectUri("http://localhost:5173/");
 
         return handler;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true); // This is required for cookies/sessions
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
